715f0dbf43efb2b8fa0b0023885df3ca58eccdd6
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-08-17 17:17:23 +0530

    Style Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,13 @@
 import Logo from "../images/Logo.png";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <header>
+    <div className={styles.layout}>
+      <header className={styles.header}>
         <img src={Logo} alt="Todo" />
       </header>
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
     </div>
   );
 };

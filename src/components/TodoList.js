7e9ad800f924ac5a5b5ea37640803e0c28f2182a
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-08-18 10:41:35 +0530

    add Add New button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,6 +4,7 @@ import styles from "./TodoList.module.css";
 const TodoList = ({ todos }) => {
   return (
     <div className={styles.todoList}>
+      <button className={styles.addButton}>Add New +</button>
       <h2>Todo List</h2>
       <ul>
         {todos.map((todo) => (

acb5e585211bcd08225b878c7b15d05963bb93ce
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-08-18 11:25:18 +0530

    add buttons for crud functions

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,18 @@
+import { FaTrash } from "react-icons/fa";
+import styles from "./TodoItem.module.css";
+
 const TodoItem = ({ title }) => {
-  return <li>{title}</li>;
+  return (
+    <div className={styles.todoItemContainer}>
+      <div className={styles.todoItem}>
+        <input type="radio" className={styles.doneCheckbox} />
+        {title}
+        <button className={styles.deleteButton}>
+          <FaTrash />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,14 @@
 import TodoItem from "./TodoItem";
 import styles from "./TodoList.module.css";
+import { FaPlus } from "react-icons/fa";
 
 const TodoList = ({ todos }) => {
   return (
     <div className={styles.todoList}>
-      <button className={styles.addButton}>Add New +</button>
+      <button className={styles.addButton}>
+        {" "}
+        Add New <FaPlus className={styles.addIcon} />
+      </button>
       <h2>Todo List</h2>
       <ul>
         {todos.map((todo) => (

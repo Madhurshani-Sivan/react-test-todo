9801bcb07c63156833666bec1d38f34c9038c7b5
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-08-21 17:28:17 +0530

    add a new task modal

---
+++ src/components/AddTaskModal.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import styles from "./AddTaskModal.module.css";
+
+const AddTaskModal = ({ onClose, onSubmit }) => {
+  const [title, setTitle] = useState("");
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (title.trim() !== "") {
+      onSubmit(title);
+      onClose();
+    }
+  };
+
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <h2>Add New Item</h2>
+        <input type="text" value={title} onChange={handleTitleChange} />
+        <button onClick={onClose}>CANCEL</button>
+        <button onClick={handleSubmit}>CONFIRM</button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTaskModal;

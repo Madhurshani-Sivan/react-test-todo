aca594d56ff7fc3d401f87144424a703d75d8bb4
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-08-22 18:19:22 +0530

    remove error boundary

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import "./App.css";
 import Layout from "./components/Layout";
 import TodoList from "./components/TodoList";
 import { TaskProvider } from "./context/TaskContext";
-import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
     <TaskProvider>
       <Layout>
-        <ErrorBoundary>
-          <TodoList />
-        </ErrorBoundary>
+        <TodoList />
       </Layout>
     </TaskProvider>
   );
